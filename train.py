fb86ca4a573bb2930545cc423a38e95502757807
PaulSantomauro <123113013+PaulSantomauro@users.noreply.github.com>
2026-03-13 13:08:15 -0400

    exp33: MLP gelu->relu (simpler, potentially more memory-efficient)

--- train.py
+++ train.py
@@ -424,7 +424,7 @@ class MLP(nn.Module):
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = F.gelu(x)
+        x = F.relu(x)
         x = self.c_proj(x)
         return x
 
@@ -450,7 +450,7 @@ class GPT(nn.Module):
             "wte": nn.Embedding(config.vocab_size, config.n_embd),
             "h": nn.ModuleList([Block(config, i) for i in range(config.n_layer)]),
         })
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=True)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         self.resid_lambdas = nn.Parameter(torch.ones(config.n_layer))
         self.x0_lambdas = nn.Parameter(torch.zeros(config.n_layer))
         head_dim = config.n_embd // config.n_head
